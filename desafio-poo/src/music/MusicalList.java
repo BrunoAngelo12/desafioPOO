70e09871d60390ad82379550d5bed0a5ee82f602
Brn <bruno.trabalhos@outlook.com>
2023-10-12 02:35:04 -0300

    criada a classe de lista musical

---
+++ desafio-poo/src/music/MusicalList.java
@@ -0,0 +1,49 @@
+package music;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class MusicalList {
+    protected Set<String> musicalList = new LinkedHashSet<>();
+    protected Set<String> favoriteList = new LinkedHashSet<>();
+
+    public void addSongToPlaylist(String music){
+        musicalList.add(music);
+        System.out.println("Song added to playlist!");
+    }
+
+    public void removeSongToPlaylist(String music){
+        for(String m : musicalList){
+            if(m.equalsIgnoreCase(music)){
+                musicalList.remove(music);
+                System.out.println("Song removed from playlist!");
+            }
+        }
+    }
+
+    public void addToFavorites(String music){
+        favoriteList.add(music);
+        System.out.println("Song added to favorites!");
+    }
+
+    public void removeSongToFavorites(String music){
+        for(String m : favoriteList){
+            if(m.equalsIgnoreCase(music)){
+                favoriteList.remove(music);
+                System.out.println("Song removed from favorites!");
+            }
+        }
+    }
+
+    public Set<String> showMusicList(){
+        return musicalList;
+    }
+
+    public Set<String> showFavoritesList(){
+        return favoriteList;
+    }
+
+
+
+
+}
