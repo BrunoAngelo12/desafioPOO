bcc01672a26f5d8dbffb0ae931cf8798a14a06f6
Brn <bruno.trabalhos@outlook.com>
2023-10-12 03:01:42 -0300

    base criada

---
+++ desafio-poo/src/Application.java
@@ -0,0 +1,8 @@
+import iphone.Iphone;
+
+public class Application {
+    public static void main(String[] args) {
+        Iphone cel = new Iphone();
+        cel.playMusic();
+    }
+}
---
+++ desafio-poo/src/iphone/Iphone.java
@@ -0,0 +1,15 @@
+package iphone;
+
+import music.MusicPlayer;
+
+public class Iphone implements MusicPlayer{
+
+    @Override
+    public void playMusic() {
+        System.out.println("Playing music..");
+    }
+
+}
+
+
+
