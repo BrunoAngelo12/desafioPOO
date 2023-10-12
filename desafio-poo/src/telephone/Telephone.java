f62cfe81deb78c4bdd3719d2853e0c7029177def
Brn <bruno.trabalhos@outlook.com>
2023-10-12 18:56:47 -0300

    criada as classes do telefone

--- desafio-poo/src/telephone/Telephone.java
+++ desafio-poo/src/telephone/Telephone.java
@@ -1,5 +1,7 @@
 package telephone;
 
 public interface Telephone {
-    
+    public void connect();
+    public void attend();
+    public void startVoicemail();
 }
--- desafio-poo/src/telephone/TelephoneDirectory.java
+++ desafio-poo/src/telephone/TelephoneDirectory.java
@@ -5,13 +5,31 @@ import java.util.Map;
 
 public class TelephoneDirectory {
     Map<Integer, String> telephoneDirectory = new HashMap<>();
+    Map<Integer, String> favoriteContacts = new HashMap<>();
 
     public TelephoneDirectory() {
         this.telephoneDirectory = new HashMap<>();
+        this.favoriteContacts = new HashMap<>();
     }
 
     public void addContactToList(int number, String name){
         telephoneDirectory.put(number, name);
+    }
+
+    public void removeContactFromlist(int number){
+        telephoneDirectory.remove(number);
+    }
+
+    public void addToFavoriteContacts(int number, String name){
+        favoriteContacts.put(number, name);
+    }
+
+    public Map<Integer, String> showContactList(){
+        return telephoneDirectory;
+    }
+
+    public Map<Integer, String> showFavoriteContactList(){
+        return favoriteContacts;
     }
 
     
